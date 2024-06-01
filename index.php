d30b65b71314d5e29942f7c7cbb36e5dac711f27
puhize <puhizarexha@hotmail.com>
2024-06-01 02:17:20 +0200

    refactored code

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-  <link href="../css/login.css" rel="stylesheet" />
+  <link href="assets/css/login.css" rel="stylesheet" />
 </head>
 
 <body>
@@ -49,9 +49,9 @@
       </div>
     </div>
   </div>
-  <script src="../js/login.js"></script>
+  <script src="assets/js/login.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-  <script src="js/scripts.js"></script>
+  <script src="assets/js/scripts.js"></script>
 </body>
 
 </html>
