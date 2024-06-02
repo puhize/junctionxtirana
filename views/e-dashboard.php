fd09f98b2947595d5d03f9c10d944f87d4eb54ce
Jona Mehmeti <jonamehmeti4@gmail.com>
2024-06-02 10:20:58 +0200

    Updated e-dashboard.

--- views/e-dashboard.php
+++ views/e-dashboard.php
@@ -28,7 +28,44 @@ if ($columnInfo) {
     <div class="wrapper ">
     <?php include('includes/sidebar.php'); ?>
         <div class="main-panel" style="height: 100vh;">
-            <?php include('includes/navbar.php'); ?>
+        <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
+    <div class="container-fluid">
+        <div class="navbar-wrapper">
+            <div class="navbar-toggle">
+                <button type="button" class="navbar-toggler">
+                    <span class="navbar-toggler-bar bar1"></span>
+                    <span class="navbar-toggler-bar bar2"></span>
+                    <span class="navbar-toggler-bar bar3"></span>
+                </button>
+            </div>
+            <a class="navbar-brand" href="javascript:;"><?php echo $_SESSION['user']['name'] . " " . $_SESSION['user']['surname'] ?></a>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navigation">
+        <ul class="navbar-nav">
+              <li class="nav-item btn-rotate dropdown">
+                <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="nc-icon nc-bell-55"></i>
+                  <p>
+                    <span class="d-lg-none d-md-block">Some Actions</span>
+                  </p>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                  <?php foreach ($notifications as $notification) : ?>
+                      <a class="dropdown-item" href="detailedNotifications.php?notificationId=<?php echo $notification['id']; ?>" ><?php echo $notification['name']; ?></a>
+                  <?php endforeach; ?>
+
+
+                </div>
+              </li>
+            </ul>
+        </div>
+    </div>
+</nav>
             <style>
                 .task {
                     display: flex;
